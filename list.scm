72840b140956617b0e9010ca1a655dfbd18901d3
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-24 19:07:23 -0700

    Create list.scm

---
+++ list.scm
@@ -0,0 +1,10 @@
+#lang racket
+(* 5 (+ 6 7  ))
+(cons 7 '( ) )
+(cons 6 (cons 7 ' ( )  ) )
+(cons + (cons 6 (cons 7 ' ( )  ) ) )
+;(cons '() (cons + (cons 6 (cons 7 ' ( )  ) ) )
+
+;this is the same as (* 5 (+ 6 7  ))
+(define s (cons'* (cons 5 (cons (cons '+ (cons '6 ( cons '7 '()))) ' ()))))
+ 
