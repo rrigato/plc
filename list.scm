e78fb49f5d1292069a174b2177aafe499b048b4f
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-24 19:36:49 -0700

    Update list.scm

--- list.scm
+++ list.scm
@@ -22,3 +22,41 @@
   ( + (squareIt x) (squareIt y))
   )
 (sum_of_squares 3 4)
+
+
+;anonymous function
+(define squareIt2
+  (lambda (x)
+    ( * x x)))
+(squareIt2 5)
+
+
+;currying a function
+(define sum_of_squares2
+   (lambda(x)
+     (lambda(y)
+       ( + (squareIt2 x) (squareIt2 y )))))
+
+( (sum_of_squares2 3)4)
+
+
+;not a list
+(cons 'a 'b)
+
+;lists
+(cons 'a (cons 'b '()))
+
+
+(define mylist ( cons 3 ( cons 4 '())))
+
+; car is like head, cdr is like tail
+(car mylist)
+(cdr mylist)
+
+(define mylist2 '(5 6) )
+mylist2
+(define mylist3 ( cons mylist mylist2))
+mylist3
+
+(cdr (cdr ( cdr mylist3)))
+(car(cdr mylist3))
