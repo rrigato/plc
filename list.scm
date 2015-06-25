03d831498912503c21aa4ad928d1b20426b4d806
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-24 19:13:36 -0700

    Update list.scm

--- list.scm
+++ list.scm
@@ -7,4 +7,10 @@
 
 ;this is the same as (* 5 (+ 6 7  ))
 (define s (cons'* (cons 5 (cons (cons '+ (cons '6 ( cons '7 '()))) ' ()))))
- 
+
+
+(define n (+ 5 6))
+
+; defining a function
+(define (squareIt x)
+  (* x x))
