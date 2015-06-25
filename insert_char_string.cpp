a303fe079d88d7e9d2ebab30197446ece6b95d99
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-24 18:48:26 -0700

    Update insert_char_string.cpp

--- insert_char_string.cpp
+++ insert_char_string.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
-//this fu
+//this assumes that the string is sorted
+//it does not change the value of the string
+//because it is pass by reference which only returns the value of a string
+//These have parameters but do not use any variables
 using namespace std;
+string insertionsort(string );
 char head (string );
 string tail (string );
 string insert (char , string  );
 int main ()
 {
-    string test = "act";
+    /*string test = "act";
     insert('s', test);
-    cout << insert ('s', test);
+    cout << insert ('s', test);*/
+    while (1)
+    {
+        string s;
+        cout << "Enter any string: " <<endl;
+        cin >>s;
+        cout << "The sorted string is " <<insertionsort(s) <<endl;
+    }
     cin.get();
     return 0;
 }
@@ -32,3 +43,12 @@ string tail (string s)
     return s.substr(1);
 }
 
+//functional version of a sorting algorithm
+string insertionsort (string s)
+{
+    if (s.empty())
+    {
+        return s;
+    }
+    return insert(head(s), insertionsort(tail(s)));
+}
