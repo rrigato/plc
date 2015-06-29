0f60aedae25d1d1123235e849385b8a511f54819
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-29 14:00:07 -0700

    Update assign1.scm

--- assign1.scm
+++ assign1.scm
@@ -1,3 +1,3 @@
-
 (define(make-random-sequence size min-value max-value)
-    (= size 0) )
+    if (= size 0) 
+    (if (>= (random max-value) min-value) (make-random-sequence (-size 1) min-value max-value) )
