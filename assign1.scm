c3d65c03f9c77de1532a3df1f77c16e93ed9d1a0
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-01 11:07:15 -0700

    Update assign1.scm

--- assign1.scm
+++ assign1.scm
@@ -1,3 +1,21 @@
 (define(make-random-sequence size min-value max-value)
     if (= size 0) 
     (if (>= (random max-value) min-value) (make-random-sequence (-size 1) min-value max-value) )
+    
+    
+    
+    (define (lmr xs)
+  (if (empty? (cdr xs)) (list '() (car xs) '()) 
+      (let* ((c (lmr (tail xs)))
+            (l (car c))
+            (m (cadr c))
+            (r (caddr c)))
+        (if (< (car xs) m)
+            (list '() (car xs) (cdr xs))
+            (list (cons (car xs) l) m r))))) 
+
+(define (selection-sort xs)
+  (if (empty? xs) xs
+      (let ((temp (lmr xs)))
+        (append (car(cdr(temp))) selection-sort(cdr(xs))))))
+(selection-sort '(a b c))
