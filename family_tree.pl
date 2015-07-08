8fd3c13b6cc4c9f7029a8ca97fe273e7e78d5782
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-08 16:18:17 -0700

    Create family_tree.pl

---
+++ family_tree.pl
@@ -0,0 +1,46 @@
+male(randy).
+male(rudy).
+male(wilson).
+male(john).
+male(jay).
+male(ryan).
+male(johnny).
+male(max).
+female(ashley).
+female(dina).
+female(heather).
+female(alice).
+female(leah).
+female(annette).
+female(jodene).
+female(patricia).
+female(krystal).
+mother(annette, ryan).
+mother(jodene, randy).
+mother(patricia, annette).
+mother(jodene, dina).
+mother(jodene, chumlee).
+mother(dina, krystal).
+mother(dina, johnny).
+mother(patrica, jay).
+mother(alice, max).
+mother(alice, leah).
+mother(alice, heather).
+father(jay, max).
+father(jay, leah).
+father(jay, heather).
+father(wilson, jay).
+father(john, johnny).
+father(john, krystal).
+father(rudy, dina).
+father(rudy, chumlee).
+father(wilson, annette).
+father(randy, ryan).
+father(rudy, randy).
+sibling(X,Y) :- parent(Z,X) , parent(Z,Y) , X\=Y.
+parent(X,Y) :- mother(X,Y).
+parent(X,Y) :- father(X,Y).
+grandparent(X,Y) :- parent(X,Z) , parent(Z,Y).
+cousin(X,Y) :- grandparent(Z,X) , grandparent(Z,Y) , X\=Y.
+uncle(X,Y) :-  cousin(Y,Z) , parent(X,Z), male(X).
+aunt(X,Y) :- cousin(Y,Z) , parent(X,Z) , female(X).
